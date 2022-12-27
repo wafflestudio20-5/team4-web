9d6ef9d0d11ceeeec3b91f319393d10ca7e5a24b
ksj-hikoo <super230299@gmail.com>
2022-12-28 06:13:29 +0900

    fix: delete console.log

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -10,7 +10,6 @@ export default function HomePage() {
 
   const handleCategorySideBar = () => {
     setopenCategorySideBar(!openCategorySideBar);
-    console.log(openCategorySideBar);
   };
 
   return (
