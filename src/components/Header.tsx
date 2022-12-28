6ea1a15acd209924f27ef94f19a952e77ae03bb6
2018-10793 <scleev@snu.ac.kr>
2022-12-28 18:32:48 +0900

    feat: Create header title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../resources/image/musinsa_logo.png';
+import styles from './Header.module.scss';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <>
+      <div className={styles.main}>
+        <Link className={styles.title} to="/">
+          <img src={logo} alt="logo" />
+        </Link>
+        <div className={styles.searchBar}></div>
+      </div>
+      <div className="header-channel">smallHeader</div>
+      <div className="header-member"></div>
+    </>
+  );
 }
--- src/types.d.ts
+++ src/types.d.ts
@@ -7,3 +7,5 @@ declare module '*.module.scss' {
   const classes: { [className: string]: string };
   export default classes;
 }
+
+declare module '*.png';
