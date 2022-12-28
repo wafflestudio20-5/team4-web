fe27debbb16f0ec0f0372f5c299f4f9ee7c92b3a
2018-10793 <scleev@snu.ac.kr>
2022-12-28 23:02:08 +0900

    feat: Create member header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -46,7 +46,7 @@ function GnbList() {
   );
 }
 
-export default function Header() {
+function MainHeader() {
   const [query, setQuery] = useState<string>('');
 
   const onSubmit = (e: React.MouseEvent<HTMLDivElement>) => {
@@ -60,38 +60,110 @@ export default function Header() {
   };
 
   return (
-    <div className={styles.header}>
-      <div className={styles.main}>
-        <Link className={styles.title} to="/">
-          <img src={logo} alt="logo" />
-        </Link>
-        <div className={styles.searchBar}>
-          <form>
-            <input
-              type="text"
-              maxLength={30}
-              value={query}
-              placeholder="인기 상품 무료 체험단 모집!"
-              onChange={onChange}
-            />
-            <div className={styles.searchButton} onClick={onSubmit}>
-              <img src={search_button} alt="search_button" />
-            </div>
-          </form>
-        </div>
-        <GnbList />
-      </div>
-      <div className={styles.channel}>
-        <span className={styles.channel_terrace}>테라스샵</span>
-        <div className={styles.channel_aside}>
-          <div className={styles.channel_raffle}>
-            래플에 참여해 보세요!
-            <span className={styles.channel_raffle_link}>래플 바로가기</span>
+    <div className={styles.main}>
+      <Link className={styles.title} to="/">
+        <img src={logo} alt="logo" />
+      </Link>
+      <div className={styles.searchBar}>
+        <form>
+          <input
+            type="text"
+            maxLength={30}
+            value={query}
+            placeholder="인기 상품 무료 체험단 모집!"
+            onChange={onChange}
+          />
+          <div className={styles.searchButton} onClick={onSubmit}>
+            <img src={search_button} alt="search_button" />
           </div>
-          <span>브랜드 구인</span>
-        </div>
+        </form>
       </div>
-      <div className="header-member"></div>
+      <GnbList />
+    </div>
+  );
+}
+
+function ChannelHeader() {
+  return (
+    <div className={styles.channel}>
+      <span className={styles.channel_terrace}>테라스샵</span>
+      <div className={styles.channel_aside}>
+        <div className={styles.channel_raffle}>
+          래플에 참여해 보세요!
+          <span className={styles.channel_raffle_link}>래플 바로가기</span>
+        </div>
+        <span>브랜드 구인</span>
+      </div>
+    </div>
+  );
+}
+
+function MemberHeader() {
+  return (
+    <div className={styles.member}>
+      <Link className={styles.member_login} to="/login">
+        로그인
+      </Link>
+      <div className={styles.member_block}>
+        <span
+          className={styles.member_link}
+          style={{
+            color: '#00a3ff',
+            fontFamily: 'Musinsa',
+            paddingTop: '6px',
+          }}
+        >
+          바로접속 ON
+        </span>
+      </div>
+      <div className={styles.member_block}>
+        <span className={styles.member_link}>마이페이지</span>
+      </div>
+      <div className={styles.member_block}>
+        <span className={styles.member_link}>최근 본 상품</span>
+      </div>
+      <div className={styles.member_block}>
+        <span
+          className={styles.member_link}
+          style={{
+            color: 'red',
+          }}
+        >
+          좋아요
+        </span>
+      </div>
+      <div className={styles.member_block}>
+        <span className={styles.member_link}>장바구니</span>
+        <span className={styles.badge}>0</span>
+      </div>
+      <div className={styles.member_block}>
+        <span className={styles.member_link}>주문배송조회</span>
+      </div>
+      <div className={styles.member_block}>
+        <span className={styles.member_link}>고객센터</span>
+      </div>
+      <div className={styles.member_block}>
+        <span
+          className={styles.member_link}
+          style={{
+            color: '#048eff',
+            fontWeight: 700,
+          }}
+        >
+          회원 가입 EVENT. 신규 가입 후 바로 사용 가능한 15% 할인 쿠폰 / 무신사
+          스탠다드 990원 구매 기회
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <MainHeader />
+      <ChannelHeader />
+      <MemberHeader />
     </div>
   );
 }
