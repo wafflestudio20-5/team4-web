66636ec442c8a875504c3ec7a660e8cc92df360e
ksj-hikoo <super230299@gmail.com>
2022-12-30 00:38:45 +0900

    fix: edit types.d.ts change during  merge

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,1 +1,13 @@
+declare module '*.module.css' {
+  const classes: { [className: string]: string };
+  export default classes;
+}
+
+declare module '*.module.scss' {
+  const classes: { [className: string]: string };
+  export default classes;
+}
+
+declare module '*.png';
+
 declare module '*.svg';
