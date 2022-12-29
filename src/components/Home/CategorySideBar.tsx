90b85c461505ceda28423683ee10ffd319ae2709
ksj-hikoo <super230299@gmail.com>
2022-12-29 16:22:35 +0900

    feat: add sidecategorybar design & add function displayCategoryinCamelCase in interface.ts & add Add.svg in asset

--- src/components/Home/CategorySideBar.tsx
+++ src/components/Home/CategorySideBar.tsx
@@ -1,5 +1,31 @@
-import styles from './index.module.css';
+import styles from './CategorySideBar.module.css';
+import {
+  Category,
+  displayCategory,
+  displayCategoryinCamelCase,
+} from '../../lib/interface';
 
 export default function CategorySideBar() {
-  return <div className={styles.categorySideBar}></div>;
+  const categorys = Object.values(Category);
+
+  return (
+    <div className={styles.categorySideBar}>
+      <div className={styles.select}>
+        <div className={styles.button}>품목</div>
+        <div className={styles.button}>브랜드</div>
+      </div>
+      <div className={styles.categorys}>
+        <span>인기</span>
+        <span className={styles.categorysEnglish}>Best</span>
+      </div>
+      {categorys.map((category) => (
+        <div className={styles.categorys}>
+          <span>{displayCategory(category)}</span>
+          <span className={styles.categorysEnglish}>
+            {displayCategoryinCamelCase(category)}
+          </span>
+        </div>
+      ))}
+    </div>
+  );
 }
