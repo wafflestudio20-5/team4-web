8b52191ca2ec069e4b76cf91b56a1192ee0eb20f
2018-10793 <scleev@snu.ac.kr>
2022-12-30 23:29:03 +0900

    feat: Attach available links in MemberHeader

--- src/components/SubHeader/MemberHeader.tsx
+++ src/components/SubHeader/MemberHeader.tsx
@@ -13,24 +13,24 @@ export default function MemberHeader({ user, onLogout }: MemberHeaderProps) {
     <div className={styles.member}>
       {user ? (
         <div className={styles.member_block}>
-          <span className={styles.member_nickname}>{user.nickname}</span>
+          <Link className={styles.member_nickname} to="/mypage/info">
+            {user.nickname}
+          </Link>
         </div>
       ) : (
         <Link className={styles.member_login} to="/login">
           로그인
         </Link>
       )}
-      <div className={styles.member_block}>
-        <span
-          className={styles.member_link}
-          style={{
-            color: '#00a3ff',
-            fontFamily: 'Musinsa',
-            paddingTop: '6px',
-          }}
-        >
-          바로접속 ON
-        </span>
+      <div
+        className={styles.member_block}
+        style={{
+          color: '#00a3ff',
+          fontFamily: 'Musinsa',
+          paddingTop: '4px',
+        }}
+      >
+        <span className={styles.member_link}>바로접속 ON</span>
       </div>
       {user && (
         <div className={styles.member_block}>
@@ -38,30 +38,34 @@ export default function MemberHeader({ user, onLogout }: MemberHeaderProps) {
         </div>
       )}
       <div className={styles.member_block}>
-        <span className={styles.member_link}>마이페이지</span>
+        <Link className={styles.member_link} to={user ? `/mypage` : `/login`}>
+          마이페이지
+        </Link>
       </div>
       <div className={styles.member_block}>
         <span className={styles.member_link}>최근 본 상품</span>
       </div>
-      <div className={styles.member_block}>
-        <span
-          className={styles.member_link}
-          style={{
-            color: 'red',
-          }}
-        >
-          좋아요
-        </span>
+      <div
+        className={styles.member_block}
+        style={{
+          color: 'red',
+        }}
+      >
+        <span className={styles.member_link}>좋아요</span>
       </div>
       <div className={styles.member_block}>
-        <span className={styles.member_link}>장바구니</span>
+        <Link className={styles.member_link} to="/cart">
+          장바구니
+        </Link>
         <span className={styles.badge}>0</span>
       </div>
       <div className={styles.member_block}>
         <span className={styles.member_link}>주문배송조회</span>
       </div>
       <div className={styles.member_block}>
-        <span className={styles.member_link}>고객센터</span>
+        <Link className={styles.member_link} to="/customercenter">
+          고객센터
+        </Link>
       </div>
       {user && (
         <div className={styles.member_block}>
@@ -70,19 +74,22 @@ export default function MemberHeader({ user, onLogout }: MemberHeaderProps) {
           </button>
         </div>
       )}
-      <div className={styles.member_block}>
-        <span
-          className={styles.member_link}
-          style={{
-            color: '#048eff',
-            fontWeight: 700,
-          }}
-        >
-          {user
-            ? `친구초대 EVENT. 친구 초대하면 무조건 5천원! 무제한 적립금 지급!`
-            : `회원 가입 EVENT. 신규 가입 후 바로 사용 가능한 15% 할인 쿠폰 / 무신사
-          스탠다드 990원 구매 기회`}
-        </span>
+      <div
+        className={styles.member_block}
+        style={{
+          color: '#048eff',
+        }}
+      >
+        {user ? (
+          <span className={styles.member_special_link}>
+            친구초대 EVENT. 친구 초대하면 무조건 5천원! 무제한 적립금 지급!
+          </span>
+        ) : (
+          <Link className={styles.member_special_link} to="/register">
+            회원 가입 EVENT. 신규 가입 후 바로 사용 가능한 15% 할인 쿠폰 /
+            무신사 스탠다드 990원 구매 기회
+          </Link>
+        )}
       </div>
     </div>
   );
