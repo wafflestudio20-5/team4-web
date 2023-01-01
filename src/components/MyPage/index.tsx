53e0bddfc957cf70a8fc07c1d5aeccdbece036af
2018-10793 <scleev@snu.ac.kr>
2023-01-01 09:43:08 +0900

    feat: Create MyPageNavigation

--- src/components/MyPage/index.tsx
+++ src/components/MyPage/index.tsx
@@ -42,10 +42,16 @@ function MyPage() {
       <Routes>
         <Route element={<MyPageLayout user={user} onLogout={onLogout} />}>
           <Route index element={<div>content</div>} />
+          <Route path="community" element={<div>/mypage/community</div>} />
           <Route path="info" element={<div>/mypage/info</div>} />
           <Route path="money" element={<div>/mypage/money</div>} />
+          <Route path="order" element={<div>/mypage/order</div>} />
           <Route path="point" element={<div>/mypage/point</div>} />
           <Route path="review" element={<div>/mypage/review</div>} />
+          <Route
+            path="viewed_goods"
+            element={<div>/mypage/viewed_goods</div>}
+          />
         </Route>
       </Routes>
     );
