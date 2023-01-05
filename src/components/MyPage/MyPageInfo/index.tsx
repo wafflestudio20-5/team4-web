732c87252b139e1b146c46c08e8d106d1a1ba123
2018-10793 <scleev@snu.ac.kr>
2023-01-06 01:24:09 +0900

    feat: Implement BasicInfo Layout

--- src/components/MyPage/MyPageInfo/index.tsx
+++ src/components/MyPage/MyPageInfo/index.tsx
@@ -1,11 +1,11 @@
 import { User } from '../../../lib/interface';
 import styles from './MyPageInfo.module.scss';
 
-interface MyPageInfoProps {
+interface BasicInfoProps {
   user: User;
 }
 
-function BasicInfo() {
+function BasicInfo({ user }: BasicInfoProps) {
   return (
     <section id="basic-info">
       <header className={styles.title}>
@@ -14,6 +14,58 @@ function BasicInfo() {
         </h1>
         <button>가려진정보 보기</button>
       </header>
+      <div className={styles.content}>
+        <div id="basic-image" className={styles.info_grid}>
+          <div
+            className={styles.grid_block}
+            style={{
+              paddingTop: '29px',
+            }}
+          >
+            사진
+          </div>
+          <div className={styles.grid_block}>
+            <img
+              className={styles.user_image}
+              src="https://image.msscdn.net/mfile_s01/_simbols/_basic/s.png"
+              alt="이미지 없음"
+            />
+            <span className={styles.image_text}>
+              회원님을 알릴 수 있는 사진을 등록해 주세요.
+              <br />
+              등록된 사진은 회원님의 게시물이나 댓글들에 사용됩니다.
+            </span>
+          </div>
+          <div className={styles.grid_block}>
+            <button className={styles.grid_button}>사진 변경</button>
+          </div>
+        </div>
+        <div id="basic-username" className={styles.info_grid}>
+          <div className={styles.grid_block}>아이디</div>
+          <div className={styles.grid_block}>
+            <strong className={styles.grid_strong}>{user.username}</strong>
+          </div>
+          <div className={styles.grid_block}></div>
+        </div>
+        <div id="basic-password" className={styles.info_grid}>
+          <div className={styles.grid_block}>비밀번호</div>
+          <div className={styles.grid_block}>
+            <strong className={styles.grid_strong}>********</strong>
+          </div>
+          <div className={styles.grid_block}>
+            <button className={styles.grid_button}>비밀번호 변경</button>
+          </div>
+        </div>
+        <div id="basic-nickname" className={styles.info_grid}>
+          <div className={styles.grid_block}>닉네임</div>
+          <div className={styles.grid_block}>
+            <strong className={styles.grid_strong}>{user.nickname}</strong>
+          </div>
+          <div className={styles.grid_block}>
+            <button className={styles.grid_button}>닉네임 변경</button>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
@@ -50,10 +102,14 @@ function DescriptionInfo() {
   );
 }
 
+interface MyPageInfoProps {
+  user: User;
+}
+
 export default function MyPageInfo({ user }: MyPageInfoProps) {
   return (
     <div className={styles.wrapper}>
-      <BasicInfo />
+      <BasicInfo user={user} />
       <AdditionalInfo />
       <div className={styles.split}>
         <SocialInfo />
