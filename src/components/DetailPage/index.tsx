ed4f94ea36e1cf7fb677d377c7e0f3d6ae3eb047
ksj-hikoo <super230299@gmail.com>
2023-01-05 20:29:31 +0900

    Feat : add logic navigate to DetailPage

--- src/components/DetailPage/index.tsx
+++ src/components/DetailPage/index.tsx
@@ -4,7 +4,6 @@ import { useParams } from 'react-router-dom';
 export default function DetailPage() {
   const param: number = Number(useParams().id);
   const item = useApiData(useApiItemFetcher(param)).data;
-  console.log(item);
 
   return (
     <>
--- src/components/MyPage/MyPageViewed/ItemViewed.tsx
+++ src/components/MyPage/MyPageViewed/ItemViewed.tsx
@@ -1,5 +1,6 @@
 import styles from './ItemViewed.module.scss';
 import { Item } from '../../../lib/interface';
+import { useNavigate } from 'react-router-dom';
 
 interface ItemPreviewProps {
   item: Item;
@@ -7,11 +8,27 @@ interface ItemPreviewProps {
 }
 
 export default function ItemViewed({ item, idx }: ItemPreviewProps) {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.itemPreview}>
-      <img className={styles.previewImage} src={item.image} alt="상품 이미지" />
+      <img
+        className={styles.previewImage}
+        src={item.image}
+        alt="상품 이미지"
+        onClick={() => {
+          navigate(`/goods/${item.id}`);
+        }}
+      />
       <div className={styles.brand}>{item.brand}</div>
-      <div className={styles.name}>{item.name}</div>
+      <div
+        className={styles.name}
+        onClick={() => {
+          navigate(`/goods/${item.id}`);
+        }}
+      >
+        {item.name}
+      </div>
       <div className={styles.price}>
         {item.newPrice ? (
           <>
