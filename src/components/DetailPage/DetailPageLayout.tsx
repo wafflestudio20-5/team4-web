621d04ef9febaa2d51036b980f05dcbf923f61b6
2018-10793 <scleev@snu.ac.kr>
2023-01-13 23:21:52 +0900

    feat: Create DetailPageHeader

--- src/components/DetailPage/DetailPageLayout.tsx
+++ src/components/DetailPage/DetailPageLayout.tsx
@@ -8,6 +8,36 @@ import {
 } from '../../lib/interface';
 import styles from './DetailPageLayout.module.scss';
 
+interface DetailPageHeaderProps {
+  name: string;
+  category: Category;
+  subCategory: SubCategory;
+}
+
+function DetailPageHeader({
+  name,
+  category,
+  subCategory,
+}: DetailPageHeaderProps) {
+  return (
+    <>
+      <div className={styles.category}>
+        <span>
+          {/* 백엔드 수정 이후에 고칠 예정 */}
+          <Link to="/">
+            {displayCategory(category.toLowerCase() as Category)}
+          </Link>
+          {' > '}
+          <Link to="/">
+            {displaySubCategory(subCategory.toLowerCase() as SubCategory)}
+          </Link>
+        </span>
+      </div>
+      <div className={styles.name}>{name}</div>
+    </>
+  );
+}
+
 interface DetailPageLayoutProps {
   item: Item;
 }
@@ -15,18 +45,11 @@ interface DetailPageLayoutProps {
 export default function DetailPageLayout({ item }: DetailPageLayoutProps) {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.category}>
-        <span>
-          {/* 백엔드 수정 이후에 고칠 예정 */}
-          <Link to="/">
-            {displayCategory(item.category.toLowerCase() as Category)}
-          </Link>
-          {' > '}
-          <Link to="/">
-            {displaySubCategory(item.subCategory.toLowerCase() as SubCategory)}
-          </Link>
-        </span>
-      </div>
+      <DetailPageHeader
+        name={item.name}
+        category={item.category}
+        subCategory={item.subCategory}
+      />
     </div>
   );
 }
