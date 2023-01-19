e24c8e17c575131ad25fd866a0d1984f87e322a8
ksj-hikoo <super230299@gmail.com>
2023-01-20 03:27:10 +0900

    Fix : resolve deploy error

--- src/components/ItemListPage/ItemList.tsx
+++ src/components/ItemListPage/ItemList.tsx
@@ -1,34 +1,26 @@
-import { useEffect, useState } from 'react';
 import { useApiData, useApiItemListFetcher } from '../../lib/api';
-import {
-  Item,
-  Category,
-  SubCategory,
-  displayCategory,
-  stringtoEnum,
-} from '../../lib/interface';
+import { Item } from '../../lib/interface';
 import ItemPreview from './ItemPreview';
 import styles from './ItemList.module.scss';
-import { useParams } from 'react-router-dom';
 
 interface ItemPreviewListProps {
   items: Item[] | null;
 }
 
-interface ItemListCategoryProps {
-  categorys: Category[];
-  selectedCategory: Category | null;
-  setSelectedCategory: React.Dispatch<React.SetStateAction<Category | null>>;
-}
+// interface ItemListCategoryProps {
+//   categorys: Category[];
+//   selectedCategory: Category | null;
+//   setSelectedCategory: React.Dispatch<React.SetStateAction<Category | null>>;
+// }
 
 export default function ItemList() {
-  const [paramCategory, setparamCategory] = useState<{
-    category: Category | null;
-    subCategory: SubCategory | undefined;
-  }>({ category: null, subCategory: undefined });
+  // const [paramCategory, setparamCategory] = useState<{
+  //   category: Category | null;
+  //   subCategory: SubCategory | undefined;
+  // }>({ category: null, subCategory: undefined });
 
-  const param = useParams();
-  const paramEnum = stringtoEnum(param.category);
+  // const param = useParams();
+  // const paramEnum = stringtoEnum(param.category);
 
   // useEffect(() => {
   //   if (param.category && param.category in Category) {
@@ -41,12 +33,7 @@ export default function ItemList() {
   // }, []);
 
   const { data: itemsData } = useApiData(
-    useApiItemListFetcher(
-      paramCategory.category,
-      paramCategory.subCategory,
-      20,
-      0
-    )
+    useApiItemListFetcher(null, undefined, 20, 0)
   );
   const items = itemsData?.items ?? null;
 
@@ -63,46 +50,46 @@ export default function ItemList() {
   );
 }
 
-function ItemListCategory({
-  categorys,
-  selectedCategory,
-  setSelectedCategory,
-}: ItemListCategoryProps) {
-  return (
-    <div className={styles.itemListCategory}>
-      <div className={styles.title}>
-        <div>실시간 랭킹</div>
-      </div>
-      <div className={styles.categorycontent}>
-        <div>
-          <button
-            key={null}
-            className={
-              selectedCategory === null ? styles.buttonselected : styles.button
-            }
-            onClick={() => setSelectedCategory(null)}
-          >
-            전체
-          </button>
-        </div>
-        {categorys.map((category) => (
-          <div key={category}>
-            <button
-              className={
-                selectedCategory === category
-                  ? styles.buttonselected
-                  : styles.button
-              }
-              onClick={() => setSelectedCategory(category)}
-            >
-              {displayCategory(category)}
-            </button>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
+// function ItemListCategory({
+//   categorys,
+//   selectedCategory,
+//   setSelectedCategory,
+// }: ItemListCategoryProps) {
+//   return (
+//     <div className={styles.itemListCategory}>
+//       <div className={styles.title}>
+//         <div>실시간 랭킹</div>
+//       </div>
+//       <div className={styles.categorycontent}>
+//         <div>
+//           <button
+//             key={null}
+//             className={
+//               selectedCategory === null ? styles.buttonselected : styles.button
+//             }
+//             onClick={() => setSelectedCategory(null)}
+//           >
+//             전체
+//           </button>
+//         </div>
+//         {categorys.map((category) => (
+//           <div key={category}>
+//             <button
+//               className={
+//                 selectedCategory === category
+//                   ? styles.buttonselected
+//                   : styles.button
+//               }
+//               onClick={() => setSelectedCategory(category)}
+//             >
+//               {displayCategory(category)}
+//             </button>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
 
 function ItemPreviewList({ items }: ItemPreviewListProps) {
   return (
