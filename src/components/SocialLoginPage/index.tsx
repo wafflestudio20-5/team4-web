e403af4c86913f5c51c804e68e1cd8920ca41a06
dongjoocha <dongjoocha@naver.com>
2023-01-21 15:48:52 +0900

    debug

--- src/components/SocialLoginPage/index.tsx
+++ src/components/SocialLoginPage/index.tsx
@@ -1,19 +1,18 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { useNavigate, useParams } from 'react-router-dom';
-import { AppDispatch } from '../../store';
-import { postRefresh } from '../../store/slices/session';
+// import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+// import { AppDispatch } from '../../store';
 
 export default function SocialLoginPage() {
-  const navigate = useNavigate();
-  const dispatch = useDispatch<AppDispatch>();
+  // const navigate = useNavigate();
+  // const dispatch = useDispatch<AppDispatch>();
   const { accessToken } = useParams();
 
   useEffect(() => {
-    dispatch(postRefresh());
+    // dispatch(postRefresh());
     // navigate('/');
     console.log(accessToken);
-  }, [dispatch, navigate, accessToken]);
+  }, [accessToken]);
 
   return <></>;
 }
