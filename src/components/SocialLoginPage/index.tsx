1fdc6ebf7eab87717ceeff0492666e8f43852757
dongjoocha <dongjoocha@naver.com>
2023-01-21 16:00:14 +0900

    Fix: misc.

--- src/components/SocialLoginPage/index.tsx
+++ src/components/SocialLoginPage/index.tsx
@@ -1,18 +1,20 @@
 import { useEffect } from 'react';
-// import { useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { AppDispatch } from '../../store';
+import { postRefresh } from '../../store/slices/session';
 // import { AppDispatch } from '../../store';
 
 export default function SocialLoginPage() {
   // const navigate = useNavigate();
-  // const dispatch = useDispatch<AppDispatch>();
+  const dispatch = useDispatch<AppDispatch>();
   const { accessToken } = useParams();
 
   useEffect(() => {
-    // dispatch(postRefresh());
+    dispatch(postRefresh());
     // navigate('/');
     console.log(accessToken);
-  }, [accessToken]);
+  }, [accessToken, dispatch]);
 
   return <></>;
 }
