8d3e1565503fea3ea024daf42ab45ca9ccaebc8d
dongjoocha <dongjoocha@naver.com>
2023-01-21 16:09:06 +0900

    Fix: cookie

--- src/components/SocialLoginPage/index.tsx
+++ src/components/SocialLoginPage/index.tsx
@@ -2,19 +2,22 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { AppDispatch } from '../../store';
-import { postRefresh } from '../../store/slices/session';
+// import { postRefresh } from '../../store/slices/session';
 // import { AppDispatch } from '../../store';
+import { useCookies } from 'react-cookie';
 
 export default function SocialLoginPage() {
   // const navigate = useNavigate();
+  const [cookies, setCookie] = useCookies(['refreshToken']);
   const dispatch = useDispatch<AppDispatch>();
   const { accessToken } = useParams();
 
   useEffect(() => {
-    dispatch(postRefresh());
     // navigate('/');
-    console.log(accessToken);
-  }, [accessToken, dispatch]);
+    setCookie('refreshToken', accessToken!);
+    console.log(cookies);
+    // dispatch(postRefresh());
+  }, [accessToken, cookies, dispatch, setCookie]);
 
   return <></>;
 }
