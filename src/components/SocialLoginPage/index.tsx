4bc573f8b7a2766a3531fe42b88870720ab0a59d
dongjoocha <dongjoocha@naver.com>
2023-01-21 15:26:43 +0900

    debug

--- src/components/SocialLoginPage/index.tsx
+++ src/components/SocialLoginPage/index.tsx
@@ -10,7 +10,7 @@ export default function SocialLoginPage() {
 
   useEffect(() => {
     dispatch(postRefresh());
-    navigate('/');
+    // navigate('/');
   }, [dispatch, navigate]);
 
   return <></>;
