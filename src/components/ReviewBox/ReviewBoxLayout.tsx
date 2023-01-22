96189eac3805fb8ce7dd3947ccba293f005162f2
TheoLee72 <theolee72@naver.com>
2023-01-23 00:46:24 +0900

    fix: simplifying params

--- src/components/ReviewBox/ReviewBoxLayout.tsx
+++ src/components/ReviewBox/ReviewBoxLayout.tsx
@@ -1,164 +1,201 @@
-import styles from "./ReviewBoxLayout.module.scss"
-import {Comment} from '../../lib/interface';
+import styles from './ReviewBoxLayout.module.scss';
+import { Comment, Review } from '../../lib/interface';
 
-import StarRate from "./StarRate";
-import React from "react";
+import StarRate from './StarRate';
+import React from 'react';
 
 interface ReviewBoxLayoutParams {
-    username: string;
-    profileImageUrl: string;
-    reviewDate: string;
-    sex: string;
-    height: number | undefined;
-    weight: number | undefined;
-    goodsImageUrl: string;
-    goodsName: string;
-    goodsOption: string | undefined;
-    reviewStar: number;
-    reviewId: number;
-    reviewContent: string;
-    size: string;
-    color: string;
-    images: string[];
-    commentCount: number;
-    comments: Comment[];
-    moreCommentBool: boolean;
-    onClick: (e: React.MouseEvent<HTMLInputElement>) => void;
+  reviewDate: string;
+  sex: string;
+  size: string;
+  color: string;
+  moreCommentBool: boolean;
+  onClick: (e: React.MouseEvent<HTMLInputElement>) => void;
+  data: Review;
 }
 interface ReviewBoxCommentParams {
-    commentProfilePic: string;
-    commentContent: string;
-    commentName: string;
-    commentDate: string;
+  commentProfilePic: string;
+  commentContent: string;
+  commentName: string;
+  commentDate: string;
 }
 
-
-function ReviewBoxComment({commentProfilePic, commentContent, commentName, commentDate}: ReviewBoxCommentParams) {
-
-    return <>
-        <div className={styles.reviewBoxComment}>
-            <div className={styles.commentProfilePic}>
-                <img src={commentProfilePic} alt="_basic/e.png"/>
-            </div>
-            <div className={styles.commentContentBox}>
-                {commentContent}
-            </div>
-            <div className={styles.commentProfileDataArea}>
-                <div className={styles.commentProfileData}>
-                    <span className={styles.commentProfileClass}><b>회원</b></span>
-                    <span className={styles.commentProfileName}>{commentName}</span>
-                    <span className={styles.commentProfileDate}>{commentDate}</span>
-                    <span className={styles.commentProfileEdit}>수정</span>
-                    <span className={styles.commentProfileDelete}>삭제</span>
-                </div>
-            </div>
+function ReviewBoxComment({
+  commentProfilePic,
+  commentContent,
+  commentName,
+  commentDate,
+}: ReviewBoxCommentParams) {
+  return (
+    <>
+      <div className={styles.reviewBoxComment}>
+        <div className={styles.commentProfilePic}>
+          <img src={commentProfilePic} alt="_basic/e.png" />
         </div>
-    </>;
+        <div className={styles.commentContentBox}>{commentContent}</div>
+        <div className={styles.commentProfileDataArea}>
+          <div className={styles.commentProfileData}>
+            <span className={styles.commentProfileClass}>
+              <b>회원</b>
+            </span>
+            <span className={styles.commentProfileName}>{commentName}</span>
+            <span className={styles.commentProfileDate}>{commentDate}</span>
+            <span className={styles.commentProfileEdit}>수정</span>
+            <span className={styles.commentProfileDelete}>삭제</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default function ReviewBoxLayout({username, profileImageUrl, reviewDate, sex, height, weight, goodsImageUrl,
-                                        goodsName, goodsOption, reviewStar, reviewId, reviewContent, size, color,
-                                        images, commentCount, comments, moreCommentBool, onClick}: ReviewBoxLayoutParams) {
-
-    const Comment = () => {
-        if (commentCount <= 2)
-        {
-            return(<>{comments.map(comment => (<ReviewBoxComment commentProfilePic={comment.user.image} commentContent={comment.content}
-                                                        commentDate={comment.createdDateTime} commentName={comment.user.nickname}></ReviewBoxComment>))}</>);
-        }
-        else
-        {
-            if (moreCommentBool === false)
-            {
-                return(<><ReviewBoxComment commentProfilePic={comments[0].user.image} commentContent={comments[0].content}
-                                           commentDate={comments[0].createdDateTime} commentName={comments[0].user.nickname}></ReviewBoxComment>
-                    <ReviewBoxComment commentProfilePic={comments[1].user.image} commentContent={comments[1].content}
-                                      commentDate={comments[1].createdDateTime} commentName={comments[1].user.nickname}></ReviewBoxComment></>);
-            }
-            else
-            {
-                return(<>{comments.map(comment => (<ReviewBoxComment commentProfilePic={comment.user.image} commentContent={comment.content}
-                                                                     commentDate={comment.createdDateTime} commentName={comment.user.nickname}></ReviewBoxComment>))}</>);
-            }
-
-        }
-
+export default function ReviewBoxLayout({
+  reviewDate,
+  sex,
+  size,
+  color,
+  moreCommentBool,
+  onClick,
+  data,
+}: ReviewBoxLayoutParams) {
+  const Comment = () => {
+    if (data.comments.length <= 2) {
+      return (
+        <>
+          {data.comments.map((comment) => (
+            <ReviewBoxComment
+              commentProfilePic={comment.user.image}
+              commentContent={comment.content}
+              commentDate={comment.createdDateTime}
+              commentName={comment.user.nickname}
+            ></ReviewBoxComment>
+          ))}
+        </>
+      );
+    } else {
+      if (moreCommentBool === false) {
+        return (
+          <>
+            <ReviewBoxComment
+              commentProfilePic={data.comments[0].user.image}
+              commentContent={data.comments[0].content}
+              commentDate={data.comments[0].createdDateTime}
+              commentName={data.comments[0].user.nickname}
+            ></ReviewBoxComment>
+            <ReviewBoxComment
+              commentProfilePic={data.comments[1].user.image}
+              commentContent={data.comments[1].content}
+              commentDate={data.comments[1].createdDateTime}
+              commentName={data.comments[1].user.nickname}
+            ></ReviewBoxComment>
+          </>
+        );
+      } else {
+        return (
+          <>
+            {data.comments.map((comment) => (
+              <ReviewBoxComment
+                commentProfilePic={comment.user.image}
+                commentContent={comment.content}
+                commentDate={comment.createdDateTime}
+                commentName={comment.user.nickname}
+              ></ReviewBoxComment>
+            ))}
+          </>
+        );
+      }
     }
-    return <div>
-        <div className={styles.reviewBox}>
-            <div className={styles.reviewProfile}>
-                <img src={profileImageUrl} className={styles.reviewProfilePic}/>
-                <div className={styles.reviewProfileTextWrap}>
-                    <div className={styles.reviewProfileText}>
-                        <p className={styles.reviewProfileName}>{username}님</p>
-                        <p className={styles.reviewProfileDate}>{reviewDate}</p>
-                    </div>
-                    <div className={styles.reviewProfileInfo}>
-                        <p className={styles.reviewProfileBodyInfo}>{sex}, {height}cm, {weight}kg</p>
-                    </div>
-                </div>
+  };
+  return (
+    <div>
+      <div className={styles.reviewBox}>
+        <div className={styles.reviewProfile}>
+          <img src={data.user.image} className={styles.reviewProfilePic} />
+          <div className={styles.reviewProfileTextWrap}>
+            <div className={styles.reviewProfileText}>
+              <p className={styles.reviewProfileName}>{data.user.nickname}님</p>
+              <p className={styles.reviewProfileDate}>{reviewDate}</p>
             </div>
-            <div className={styles.reviewGoodsInformation}>
-                <div className={styles.reviewGoodsThumbnail}>
-                    <img src={goodsImageUrl}
-                         className={styles.reviewGoodsPic}/>
-                </div>
-                <div className={styles.reviewGoodsItem}>
-                    <span className={styles.reviewGoodsName}>
-                        {goodsName}
-                    </span>
-                    <p className={styles.reviewGoodsOption}>-{goodsOption} 구매</p>
-
-                </div>
+            <div className={styles.reviewProfileInfo}>
+              <p className={styles.reviewProfileBodyInfo}>
+                {sex}, {data.user.height}cm, {data.user.weight}kg
+              </p>
             </div>
-            <div className={styles.reviewRatingWrap}>
-                <StarRate rating_score={reviewStar} review_id={reviewId} />
-            </div>
-
-            <div className={styles.reviewContent}>
-                <div className={styles.reviewContentText}>
-                    {reviewContent}
-                </div>
-                <div className={styles.reviewContentEvaluation}>
-                    <ul className={styles.reviewEvaluationList}>
-                        <li className={styles.reviewEvaluationItem}>
-                            사이즈&nbsp;
-                            <span>{size}</span>
-                        </li>
-                        <li className={styles.reviewEvaluationItem}>
-                            색감&nbsp;
-                            <span>{color}</span>
-                        </li>
-                    </ul>
-                </div>
-                <div className={styles.reviewContentImage}>
-                    <div className={styles.reviewContentImageWrap}>
-                        <ul className={styles.reviewContentImageList}>
-                            {images.map(image=>(<li className={styles.reviewContentImageItem} style={{backgroundImage:`url(${image})`}}> </li>))}
-
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div className={styles.reviewCommentCount}>
-                댓글&nbsp;
-                <span>{commentCount}개</span>
-            </div>
-            <div className={styles.reviewCommentList}>
-                <div className={styles.reviewCommentInput}>
-                    <div className={styles.reviewCommentFakeInputBox}></div>
-                    <span className={styles.reviewCommentFakeSubmitButton}>댓글작성</span>
-                </div>
-                <Comment></Comment>
-                {/*review2개 넘으면 댓글 더보기 버튼*/}
-                {moreCommentBool ? null: <div className={styles.reviewMoreWrap}>
-                    <div className={styles.reviewMore}>
-                        <div className={styles.reviewMoreText} onClick={onClick}>
-                            댓글 더보기
-                        </div>
-                    </div>
-                </div>}
-            </div>
+          </div>
         </div>
-    </div>;
+        <div className={styles.reviewGoodsInformation}>
+          <div className={styles.reviewGoodsThumbnail}>
+            <img
+              src={data.purchase.item.images[0]}
+              className={styles.reviewGoodsPic}
+            />
+          </div>
+          <div className={styles.reviewGoodsItem}>
+            <span className={styles.reviewGoodsName}>
+              {data.purchase.item.name}
+            </span>
+            <p className={styles.reviewGoodsOption}>
+              -{data.purchase.option} 구매
+            </p>
+          </div>
+        </div>
+        <div className={styles.reviewRatingWrap}>
+          <StarRate rating_score={data.rating} review_id={data.id} />
+        </div>
+
+        <div className={styles.reviewContent}>
+          <div className={styles.reviewContentText}>{data.content}</div>
+          <div className={styles.reviewContentEvaluation}>
+            <ul className={styles.reviewEvaluationList}>
+              <li className={styles.reviewEvaluationItem}>
+                사이즈&nbsp;
+                <span>{size}</span>
+              </li>
+              <li className={styles.reviewEvaluationItem}>
+                색감&nbsp;
+                <span>{color}</span>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.reviewContentImage}>
+            <div className={styles.reviewContentImageWrap}>
+              <ul className={styles.reviewContentImageList}>
+                {data.images.map((image) => (
+                  <li
+                    className={styles.reviewContentImageItem}
+                    style={{ backgroundImage: `url(${image})` }}
+                  >
+                    {' '}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className={styles.reviewCommentCount}>
+          댓글&nbsp;
+          <span>{data.comments.length}개</span>
+        </div>
+        <div className={styles.reviewCommentList}>
+          <div className={styles.reviewCommentInput}>
+            <div className={styles.reviewCommentFakeInputBox}></div>
+            <span className={styles.reviewCommentFakeSubmitButton}>
+              댓글작성
+            </span>
+          </div>
+          <Comment></Comment>
+          {/*review2개 넘으면 댓글 더보기 버튼*/}
+          {moreCommentBool ? null : (
+            <div className={styles.reviewMoreWrap}>
+              <div className={styles.reviewMore}>
+                <div className={styles.reviewMoreText} onClick={onClick}>
+                  댓글 더보기
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/ReviewBox/index.tsx
+++ src/components/ReviewBox/index.tsx
@@ -1,65 +1,53 @@
-import ReviewBoxLayout from "./ReviewBoxLayout";
-import {useState} from 'react';
-import {Review} from '../../lib/interface';
+import ReviewBoxLayout from './ReviewBoxLayout';
+import { useState } from 'react';
+import { Review } from '../../lib/interface';
 
-interface DataType {
-    data: Review;
+interface ReviewBoxParams {
+  data: Review;
 }
 
-export default function ReviewBox({data}: DataType) {
-    const sexText = () => {
-        if (data.user.sex === 'male')
-        {
-            return '남성';
-        }
-        else if (data.user.sex === 'female')
-        {
-            return '여성';
-        }
-        else
-        {
-            return '';
-        }
+export default function ReviewBox({ data }: ReviewBoxParams) {
+  const sexText = () => {
+    if (data.user.sex === 'male') {
+      return '남성';
+    } else if (data.user.sex === 'female') {
+      return '여성';
+    } else {
+      return '';
     }
-    const sizeText = () => {
-        if (data.size === 'large')
-        {
-            return '커요';
-        }
-        else if (data.size === 'mid')
-        {
-            return '보통이에요';
-        }
-        else
-        {
-            return '작아요';
-        }
+  };
+  const sizeText = () => {
+    if (data.size === 'large') {
+      return '커요';
+    } else if (data.size === 'mid') {
+      return '보통이에요';
+    } else {
+      return '작아요';
     }
-    const colorText = () => {
-        if (data.color === 'bright')
-        {
-            return '밝아요';
-        }
-        else if (data.color === 'mid')
-        {
-            return '보통이에요';
-        }
-        else
-        {
-            return '어두워요';
-        }
+  };
+  const colorText = () => {
+    if (data.color === 'bright') {
+      return '밝아요';
+    } else if (data.color === 'mid') {
+      return '보통이에요';
+    } else {
+      return '어두워요';
     }
-    const [moreCommentBool, setMoreCommentBool] = useState<boolean>(false);
-    const onClick = () => {
-        setMoreCommentBool(true);
-    }
+  };
+  const [moreCommentBool, setMoreCommentBool] = useState<boolean>(false);
+  const onClick = () => {
+    setMoreCommentBool(true);
+  };
 
-
-
-
-    return (<ReviewBoxLayout
-    username={data.user.nickname} profileImageUrl={data.user.image} reviewDate={data.createdDateTime} sex={sexText()
-    } height={data.user.height} weight={data.user.weight} goodsImageUrl={data.purchase.item.images[0]} goodsName={data.purchase.item.name} goodsOption={data.purchase.option}
-    reviewStar={data.rating} reviewId={data.id} reviewContent={data.content} size={sizeText()} color={colorText()} images={data.images}
-    commentCount={data.comments.length} comments={data.comments} moreCommentBool={moreCommentBool} onClick={onClick}></ReviewBoxLayout>);
+  return (
+    <ReviewBoxLayout
+      reviewDate={data.createdDateTime}
+      sex={sexText()}
+      size={sizeText()}
+      color={colorText()}
+      moreCommentBool={moreCommentBool}
+      onClick={onClick}
+      data={data}
+    ></ReviewBoxLayout>
+  );
 }
