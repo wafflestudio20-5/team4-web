bf536fec4d27a6bc3a1e4bd0f290bfbaf8ae0e58
TheoLee72 <theolee72@naver.com>
2023-01-23 13:09:27 +0900

    feat: empty component

---
+++ src/components/InquiryPopUpPost/InquiryPopUpPostLayout.tsx
@@ -0,0 +1,5 @@
+import styles from "./InquiryPopUpPostLayout.module.scss";
+export default function InquiryPopUpPostLayout() {
+
+    return();
+}
---
+++ src/components/InquiryPopUpPost/index.tsx
@@ -0,0 +1,4 @@
+import InquiryPopUpPostLayout from './InquiryPopUpPostLayout';
+export default function InquiryPopUpPost() {
+  return <InquiryPopUpPostLayout></InquiryPopUpPostLayout>;
+}
