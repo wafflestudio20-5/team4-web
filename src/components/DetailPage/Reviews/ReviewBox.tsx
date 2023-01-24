1d0ba52ba4bfba87fdc8ad67117ffe03032d3cb2
2018-10793 <scleev@snu.ac.kr>
2023-01-25 01:38:14 +0900

    feat: Implement purchase info section

--- src/components/DetailPage/Reviews/ReviewBox.tsx
+++ src/components/DetailPage/Reviews/ReviewBox.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './ReviewBox.module.scss';
 
 export default function ReviewBox() {
@@ -19,6 +20,24 @@ export default function ReviewBox() {
           <div className={styles.profile_info}>
             <p>남성, 170cm, 70kg</p>
           </div>
+        </div>
+      </div>
+      <div className={styles.purchase_info}>
+        <div className={styles.purchase_item_image}>
+          {/* 추후 수정 필요*/}
+          <Link to="/">
+            <img
+              src="https://image.msscdn.net/images/goods_img/20220829/2752332/2752332_1_100.jpg"
+              alt=""
+            />
+          </Link>
+        </div>
+        <div className={styles.purchase_item_text}>
+          {/* 추후 수정 필요*/}
+          <Link to="/" className={styles.purchase_item_info}>
+            빅 어워드 스타디움 울 바시티자켓 블랙
+          </Link>
+          <p className={styles.purchase_option_info}>XL 구매</p>
         </div>
       </div>
     </div>
