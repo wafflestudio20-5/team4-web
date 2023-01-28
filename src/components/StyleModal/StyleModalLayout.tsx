064a3a180df7e67110b2ef3fc7db6b69c4ee163e
2018-10793 <scleev@snu.ac.kr>
2023-01-28 20:01:38 +0900

    feat: Revise StyleModalLayout

--- src/components/StyleModal/StyleModalLayout.tsx
+++ src/components/StyleModal/StyleModalLayout.tsx
@@ -1,9 +1,32 @@
 import styles from './StyleModalLayout.module.scss';
 
+function StyleModalImages() {
+  return <div className={styles.images}></div>;
+}
+
+function StyleModalBody() {
+  return (
+    <div className={styles.content}>
+      <div className={styles.header}>
+        <div className={styles.profile}>
+          <img
+            className={styles.profile_image}
+            src="https://image.msscdn.net/mfile_s01/_simbols/_basic/k.png"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function StyleModalLayout() {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.modal}></div>
+      <div className={styles.modal}>
+        <StyleModalImages />
+        <StyleModalBody />
+      </div>
     </div>
   );
 }
