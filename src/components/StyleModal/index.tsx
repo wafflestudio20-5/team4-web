4017276f250452e827e74b202ee4c6dfd871f0b6
2018-10793 <scleev@snu.ac.kr>
2023-01-28 22:08:26 +0900

    feat: Create modalSlice

--- src/components/StyleModal/index.tsx
+++ src/components/StyleModal/index.tsx
@@ -1,11 +1,5 @@
-import { Outlet } from 'react-router-dom';
 import StyleModalLayout from './StyleModalLayout';
 
 export default function StyleModal() {
-  return (
-    <>
-      <StyleModalLayout />
-      <Outlet />
-    </>
-  );
+  return <StyleModalLayout />;
 }
---
+++ src/store/slices/modal.ts
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface ModalState {
+  open: boolean;
+  styleId?: number;
+}
+
+const initialState: ModalState = {
+  open: false,
+};
+
+export const modalSlice = createSlice({
+  name: 'modal',
+  initialState,
+  reducers: {
+    setOpen: (state, action: PayloadAction<number>) => {
+      state.open = true;
+      state.styleId = action.payload;
+    },
+    setClose: (state) => {
+      state.open = false;
+      state.styleId = undefined;
+    },
+  },
+});
+
+export const modalActions = modalSlice.actions;
+export default modalSlice;
