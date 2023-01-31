5d511c9bb0f5dfce788f0adf41416c4108af11f2
ksj-hikoo <super230299@gmail.com>
2023-01-31 20:28:18 +0900

    Feat: ItemListPage & StyleListPage Pagenation

--- src/components/Closet/ClosetMy.tsx
+++ src/components/Closet/ClosetMy.tsx
@@ -1,14 +1,24 @@
 import ClosetHeader from './ClosetHeader';
 import ClosetBody from './ClosetBody';
 import { User } from '../../lib/interface';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default function ClosetOther({
+export default function ClosetMy({
   user,
   accessToken,
 }: {
   user: User | null;
   accessToken: string | null;
 }) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/login');
+    }
+  });
+
   const parsedId = user?.id ?? null;
   const isMe: boolean = true;
 
