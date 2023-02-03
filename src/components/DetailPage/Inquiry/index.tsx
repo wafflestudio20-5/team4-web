cc969e8ff46a1f9e18bdf4e712ea9132664d09b0
2018-10793 <scleev@snu.ac.kr>
2023-02-04 04:08:15 +0900

    feat: Implement item inquiry display

--- src/components/DetailPage/Inquiry/index.tsx
+++ src/components/DetailPage/Inquiry/index.tsx
@@ -1,3 +1,7 @@
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import { RootState } from '../../../store';
 import { Inquiry } from '../../../lib/interface';
 import InquiryBoxLayout from './InquiryBoxLayout';
 
@@ -6,5 +10,21 @@ interface InquiryBoxProps {
 }
 
 export default function InquiryBox({ inquiry }: InquiryBoxProps) {
-  return <InquiryBoxLayout inquiry={inquiry} />;
+  const [verbose, setVerbose] = useState<boolean>(false);
+
+  const { user } = useSelector((state: RootState) => {
+    return state.session;
+  });
+
+  const onSwitch = (e: React.MouseEvent<HTMLSpanElement>) => {
+    if (inquiry.isSecret && inquiry.user.id !== user?.id) {
+      toast('비밀글은 작성자만 볼 수 있습니다.');
+      return;
+    }
+    setVerbose(!verbose);
+  };
+
+  return (
+    <InquiryBoxLayout inquiry={inquiry} verbose={verbose} onSwitch={onSwitch} />
+  );
 }
